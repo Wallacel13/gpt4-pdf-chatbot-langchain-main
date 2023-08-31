7835e09ee1b155a378f74f5a2fc53d83137039e0
WallCosta <wallacel.1398@gmail.com>
2023-08-31 16:48:42 -0300

    Delete .env.ts

--- .env.ts
+++
@@ -1,8 +0,0 @@
-OPENAI_API_KEY=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
-
-# Update these with your pinecone details from your dashboard. 
-# PINECONE_INDEX_NAME is in the indexes tab under "index name" in blue
-# PINECONE_ENVIRONMENT is in indexes tab under "Environment". Example: "us-east1-gcp"
-PINECONE_API_KEY=f909f8d5-03ea-424c-9ce5-ad1cf34dcfe0
-PINECONE_ENVIRONMENT=us-west4-gcp-free
-PINECONE_INDEX_NAME=pdf-test 
